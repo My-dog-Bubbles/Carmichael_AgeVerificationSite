cfbbb9e84660345d493f741dfdbed909131a637e
Angel Carmichael <amcarmichael2@my.waketech.edu>
2025-01-28 14:03:18 -0500

    good luck

---
+++ index.js
@@ -0,0 +1,6 @@
+let age
+const Question = document.getElementById("Question").textcontent = "What is your name: "
+const user_input = document.getElementById("userAge").textcontent = " "
+const submit = document.getElementById("submit").textcontent.addEventListener("click". function(submit = age))
+
+document.write(Question)
